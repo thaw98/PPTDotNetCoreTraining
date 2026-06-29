b1a5cc5649da9fd7c5b9d04fa58b46e3275c4a9a
Pyae Phyo Thaw <ppthaw1998@gmail.com>
2026-06-29 14:34:07 +0630

    feat: add db first + connect with class library

---
+++ DotNetTrainingBatch5.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainingBatch5.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            return Ok(new {Message = "GetBlogs"});
+        }
+        [HttpPost]
+        public IActionResult CreateBlog()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateBlog()
+        {
+            return Ok();
+        }
+        [HttpPatch]
+        public IActionResult PatchBlog()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteBlog()
+        {
+            return Ok();
+        }
+    }
+}
